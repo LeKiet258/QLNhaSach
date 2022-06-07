f23df8795b38c3ee889352f5b55817769a1dc967
Le Kiet <lekietvn@gmail.com>
2022-06-07 11:22:00 +0700

    exclude heroku setting

--- QLNhaSach/settings.py
+++ QLNhaSach/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+# import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -134,4 +134,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static').replace('\\', '/')
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
